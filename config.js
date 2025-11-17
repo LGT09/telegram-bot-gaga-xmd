a2eeab43b649a7e745bf3063002a64d6e4fbe014
Vincent Ganiza <vincentganiza9@gmail.com>
2025-11-17 22:35:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 global.prefix = '/'; // Set Your Preferred Prefix
-global.botName = 'GIFTED-MD'; // Bot Name
-global.botVersion = '2.0.2';
-global.botPic = 'https://files.giftedtech.web.id/file/gifted-md.jpg'; // Can use path, can also use link
-global.botToken = '7267479440:AAFUOGy7mqGNCI8klIkOqb0QzYoYCQ9o8Rw'; // Replace With Your Bot Token (Get it from BotFather)
-global.ownerUsername = 'mauricegift'; // Replace With Your Telegram Username(For Bot Owner)
-global.timeZone = 'Africa/Nairobi'; // Your Time Zone
-global.ownerId = [8084255827]; // Replace With Your Owner Chat ID(For Bot Owner), You Can Add More than 1
-global.footer = '*Powered by GiftedTech*';
-global.ytdlWeb = 'https://youtube.giftedtech.web.id';
+global.botName = 'GAGA-MD; // Bot Name
+global.botVersion = '1.0.0';
+global.botPic = 'https://files.catbox.moe/hvljlp.jpg'; // Can use path, can also use link
+global.botToken = '8456978693:AAGwEnVcdzqwzVOR5wAohRXBy7_iz9m8JxU'; // Replace With Your Bot Token (Get it from BotFather)
+global.ownerUsername = '@xaphnathpanior; // Replace With Your Telegram Username(For Bot Owner)
+global.timeZone = 'Africa/Harare'; // Your Time Zone
+global.ownerId = [8456978693]; // Replace With Your Owner Chat ID(For Bot Owner), You Can Add More than 1
+global.footer = '*Powered by Traxxion Inc Tech*';
+global.ytdlWeb = 'https://youtube.com/@lilgagaofc?si=_iwXETiXIcp9Gs1E';
 global.giftedYtdlpApi = 'https://ytapi.giftedtech.co.ke';
 global.giftedApi = 'https://api.giftedtech.co.ke/api'; // Gifted Api
 global.giftedKey = 'gifted'; // Gifted Api Key
 global.lyffeApi = 'https://levanter.onrender.com'; // Lyffe Api
-global.giftedWaChannel = 'https://whatsapp.com/channel/0029Vb3hlgX5kg7G0nFggl0Y';
-global.giftedRepo = "https://github.com/mauricegift/telegram-bot";
-global.giftedApiRepo = "https://api.github.com/repos/mauricegift/telegram-bot";
+global.giftedWaChannel = 'https://whatsapp.com/channel/0029Vb6njtcG3R3n7HS5Vs0P';
+global.giftedRepo = "https://github.com/LGT09/gaga09-xmd-wa-bot";
+global.giftedApiRepo = "https://api.github.com/repos/LGT09/gaga09-xmd-wa-bot";
 
 global.giftechMess = {
     internalerr: 'Internal Server Error!',
